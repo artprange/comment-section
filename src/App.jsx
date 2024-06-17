c98a93a8874f287f35c23b38977865c01b002371
Arthur Prange <arthur.prange@gmail.com>
2024-06-17 16:48:53 -0300

    aside

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { Post } from './Post'
 import { Header } from './components/Header'
 
+import styles from './App.module.css'
 import './global.css'
+import { Sidebar } from './components/Sidebar'
 
 
 export  function App() {
@@ -10,6 +12,24 @@ export  function App() {
   return (
     <div>
     <Header/>
+
+    <div className={styles.wrapper}>
+      <aside>
+        <Sidebar/>
+      </aside>
+      <main>
+
+        <Post
+         author="Kahuna"
+         content="asdbakijs bdakis dbaskodjbaksd jbas dbjasl dbas das"
+        />
+        <Post
+         author="Athena"
+         content="puyjtpyolijmtpoyjum optyjmoptj mp"/>
+
+      </main>
+    </div>
+
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import{ PencilLine } from 'phosphor-react'
+import styles from './Sidebar.module.css';
+
+
+export function Sidebar(){
+    return(
+        
+            <aside className={styles.sidebar}> 
+            <img className={styles.cover}
+             src="https://images.unsplash.com/photo-1536961229726-d383212033cb?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+             />
+
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/104018176?v=4"/>
+                <strong>Kahuna</strong>
+                <span>Web Dev</span>
+            </div>
+
+            <footer className={styles.footer}>
+             
+                <a href="#">
+                <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+            </aside>
+        
+    )
+}
